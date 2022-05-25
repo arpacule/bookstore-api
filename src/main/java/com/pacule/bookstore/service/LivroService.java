0e8affee5b1c0d48825cf8e30bf1120c9917f865
arpacule <arpacule@gmail.com>
2022-05-25 21:04:46 +0200

    Get Para Livros

--- src/main/java/com/pacule/bookstore/service/LivroService.java
+++ src/main/java/com/pacule/bookstore/service/LivroService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pacule.bookstore.dominio.Categoria;
 import com.pacule.bookstore.dominio.Livro;
 import com.pacule.bookstore.repository.LivroRepository;
 import com.pacule.bookstore.service.exceptions.ObjectNotFoundException;
@@ -16,19 +18,20 @@ public class LivroService {
 	private LivroRepository repository;
 	@Autowired
 	private CategoriaService categoriaservice;
-	
+
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto nao encontrdo id: "+id+" Tipo: "+ Livro.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrdo id: " + id + " Tipo: " + Livro.class.getName()));
 	}
-	
-	public List<Livro> findAll(Integer id_cat){
-		categoriaservice.findById(id_cat);	
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaservice.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
 	}
 
 	public Livro update(Integer id, Livro obj) {
-		Livro newObj=findById(id);
+		Livro newObj = findById(id);
 		UpdateData(newObj, obj);
 		return repository.save(newObj);
 	}
@@ -37,6 +40,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setTexto(obj.getTexto());
 		newObj.setNome_autor(obj.getNome_autor());
-		
+
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaservice.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
